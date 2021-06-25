236ea3c9e499cddbbd603704ddedc1d8e27c54ec
NivaldoCouto <nivaldo_couto@hotmail.com>
2021-06-24 22:08:04 -0300

    initialized application home html and scss area

---
+++ src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import LogoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+import '../styles/auth.scss';
+
+export function Home() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="ilustração perguntas e respostas" />
+                <h2>Crie salas de Q&amp;A ao-vivo</h2>
+                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div>
+                    <img src={LogoImg} alt="Letmeask" />
+                    <button>
+                        <img src={googleIconImg} alt="" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div> ou entre em uma sala</div>
+                </div>
+                <form action="">
+                    <input
+                        type="text"
+                        placeholder="Digite o código da sala"
+                    />
+                    <button type="submit">
+                        Entrar na sala
+                    </button>
+                </form>
+            </main>
+        </div>
+
+    )
+}
